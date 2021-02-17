330b416752a9fe2efd1d4449421650c85eeb33a9
Aniket Rathi <aniketrathi897@gmail.com>
2021-02-17 14:42:05 -0500

    server setup

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+dotenv.config();
+
+// set up server
+
+const app = express();
+
+const mongoDB = process.env.DEV_DB_URL;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
