127d043e3a3f84a7b3d67f80c7334daaa2c09100
Aniket Rathi <aniketrathi897@gmail.com>
2021-02-18 12:38:28 -0500

    added middleware

--- server/routes/blog-route.js
+++ server/routes/blog-route.js
@@ -1,21 +1,36 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
 
+const auth = require("../middlewares/auth-middleware");
 const Blog = require("../models/blog");
+const blog_validator = require("../validators/blog-validator");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  try {
-    const { title, description } = req.body;
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errorMessage: errors.array() });
+router.post(
+  "/",
+  auth.auth,
+  blog_validator.generateValidator,
+  async (req, res) => {
+    try {
+      const { title, description } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errorMessage: errors.array() });
+      }
+
+      const blog = await new Blog({
+        title,
+        description,
+        author: req.user,
+      });
+      const savedBlog = await blog.save();
+      res.json(savedBlog);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send();
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send();
   }
-});
+);
 
 module.exports = router;
