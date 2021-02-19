f18c4567c3464718a59330d800bace2e3702ad79
Aniket Rathi <aniketrathi897@gmail.com>
2021-02-18 20:52:42 -0500

    added context for logged in

--- client/src/app.js
+++ client/src/app.js
@@ -2,10 +2,16 @@ import axios from "axios";
 import React from "react";
 import Router from "./router";
 
+import { AuthContextProvider } from "./context/auth-context";
+
 axios.defaults.withCredentials = true;
 
 function App() {
-  return <Router />;
+  return (
+    <AuthContextProvider>
+      <Router />
+    </AuthContextProvider>
+  );
 }
 
 export default App;
