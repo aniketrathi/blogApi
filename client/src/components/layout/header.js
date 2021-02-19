ef83f4024343ca5117d4b320526a042b37091536
Aniket Rathi <aniketrathi897@gmail.com>
2021-02-18 21:08:50 -0500

    updated navbar

--- client/src/components/layout/header.js
+++ client/src/components/layout/header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   Collapse,
   Navbar,
@@ -9,27 +9,37 @@ import {
   NavItem,
 } from "reactstrap";
 
+import AuthContext from "../../context/auth-context";
+
 const Header = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
+  const { loggedIn } = useContext(AuthContext);
+
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/"><h3>Blogs</h3></NavbarBrand>
+        <NavbarBrand href="/">
+          <h3>Blogs</h3>
+        </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
               <NavLink href="/">Home</NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink href="/register">Register</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/login">Login</NavLink>
-            </NavItem>
+            {loggedIn === false && (
+              <>
+                <NavItem>
+                  <NavLink href="/register">Register</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/login">Login</NavLink>
+                </NavItem>
+              </>
+            )}
           </Nav>
         </Collapse>
       </Navbar>
