b58d4112954a793aeb89a9a18b2c2d55e1953376
Aniket Rathi <aniketrathi897@gmail.com>
2021-02-18 21:17:24 -0500

    updated router

--- client/src/router.js
+++ client/src/router.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import Login from "./components/auth/login/login";
 import Register from "./components/auth/register/register";
 import Header from "./components/layout/header";
+import AuthContext from "./context/auth-context";
 
 function Router() {
+  const { loggedIn } = useContext(AuthContext);
   return (
     <BrowserRouter>
       <Header />
@@ -12,12 +15,16 @@ function Router() {
         <Route exact path="/">
           <div>Home</div>
         </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
+        {loggedIn === false && (
+          <>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </>
+        )}
       </Switch>
     </BrowserRouter>
   );
